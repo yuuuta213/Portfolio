a98752c059db4d4d5aaddcfb4da2b12e2732add5
yuta <yuuuta213@gmail.com>
2023-08-27 14:50:23 +0900

    refactor

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -40,7 +40,7 @@ const CarCard = ({ car }: CarCardProps) => {
       </div>
 
       <div className="relative flex w-full mt-2">
-        <div className="flex group-gover:invisible w-full justify-between text-gray">
+        <div className="flex group-hover:invisible w-full justify-between text-gray">
 
           <div className="flex flex-col justify-center items-center gap-2">
             <Image src="/steering-wheel.svg" width={20} height={20} alt="steering wheel" />
--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import { CustomButtonProps } from '@/types';
 import Image from 'next/image';
+import { CustomButtonProps } from '@/types';
 
-const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles, rightIcon }: CustomButtonProps) => {
+const CustomButton = ({ isDisabled,title, containerStyles, handleClick, btnType, textStyles, rightIcon }: CustomButtonProps) => {
   return (
     <button
-      disabled={false}
+      disabled={isDisabled}
       type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
@@ -18,7 +18,7 @@ const CustomButton = ({ title, containerStyles, handleClick, btnType, textStyles
         <div className="relative w-6 h-6">
           <Image
             src={rightIcon}
-            alt="right icon"
+            alt="arrow_left"
             fill
             className="object-contain"
           />
